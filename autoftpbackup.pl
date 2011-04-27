abb1bbd07f1e76a28c12d2cd8851d68029a162d4
paul <daihuxyz@yahoo.com.cn>
2011-04-27 14:52:33 +0800

    delete .project

--- autoftpbackup.pl
+++ autoftpbackup.pl
@@ -8,9 +8,4 @@ $password="3xzZCoidvs";
 $ftp->login($username,$password) or die "Could not connect.\n";
 $ftp->pwd();
 
-#$remotefile = "index.php";
-# $localfile = "autoftpbackup.pl";
-# $ftp->put($localfile,$remotefile) or die "Could not put remotefile:$remotefile\n";
-#$ftp->get($remotefile) or die "Could not put localfile:$localfile\n";
-
 
