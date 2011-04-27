d3d688825e58d439b777427e8a68a861498ec576
paul <daihuxyz@yahoo.com.cn>
2011-04-27 15:40:21 +0800

    beta

--- autoftpbackup.pl
+++ autoftpbackup.pl
@@ -1,10 +1,10 @@
-#!/usr/bin/perl -w
-#file autoftpbackup.pl
-#auto backup file
+#! /usr/bin/perl
 use Net::FTP;
-$ftp = Net::FTP->new("174.121.21.12",Passive=>1)or die "Cannot connect to some.host.name: $@";
-$ftp->login("daihu",'3xzZCoidvs')or die "Cannot login ", $ftp->message;
-$ftp->cwd("/www")or die "Cannot change working directory ", $ftp->message;
-#$ftp->dir();
-$ftp->get("index.php")or die "get failed ", $ftp->message;
-$ftp->quit;
+$ftp = Net::FTP->new("174.121.21.12", Timeout => 30) or die "Could not connect.\n";
+$username = "daihu";
+$password = "3xzZCoidvs";
+$ftp->login($username, $password) or die "Could not log in.\n";
+$ftp->cwd("/www");
+$remotefile = "index.php";
+$localfile = "file.txt";
+$ftp->get($remotefile, $localfile) or die "Can not get file.\n"
