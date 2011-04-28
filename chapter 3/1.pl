3637917613d6b871e38314f7170d97f04c27d9b4
paul <daihuxyz@yahoo.com.cn>
2011-04-28 13:52:50 +0800

    add perl first

---
+++ chapter 3/1.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+use warnings;
+#use diagnostics;
+#use strict;
+#use Carp;
+print "What is you name ?\n";
+chomp($name = <STDIN>);
+print "Welcome ,$name ,are you ready to learn perl now ?";
