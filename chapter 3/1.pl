57be06be5ea6bc6551a728357972e499d2847922
paul <daihuxyz@yahoo.com.cn>
2011-04-28 14:23:33 +0800

    add eq and or funtional

--- chapter 3/1.pl
+++ chapter 3/1.pl
@@ -1,8 +1,18 @@
 #!/usr/bin/perl
 use warnings;
-#use diagnostics;
+use diagnostics;
 #use strict;
-#use Carp;
+use Carp;
 print "What is you name ?\n";
 chomp($name = <STDIN>);
-print "Welcome ,$name ,are you ready to learn perl now ?";
+print "Welcome ,$name ,are you ready to learn perl now ?\n";
+chomp($response=<STDIN>);
+if ($response eq "yes" or $response eq "y"){
+	print "Great! let's get started learning perl by example.\n";
+	
+}
+else {
+	print "OK!!!!! Try again later";
+}
+$now=localtime();
+print "$name you can this script on $now";
