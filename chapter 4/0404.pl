c718180c7980cf39437a5a190adfb966c3d1a129
paul <daihuxyz@yahoo.com.cn>
2011-05-04 13:56:29 +0800

    name and \n evaluated

---
+++ chapter 4/0404.pl
@@ -0,0 +1,8 @@
+$name="Ellie";
+print "Hello, $name.\n";	# $name and \n evaluated
+print 'Hello, $name.\n';	# String is literal; newline not
+	# interpreted
+
+print "I don't care!\n";	# \n is interpreted in double quotes
+print 'I don\'t care!', "\n";	# Backslash protects single quote 
+	
