0b3c31b505fa374803d5487794f12bbf466b438e
paul <daihuxyz@yahoo.com.cn>
2011-05-04 18:27:26 +0800

    #!/usr/bin/perl use warnings; use diagnostics;

---
+++ chapter 5/0525.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+use warnings;
+use diagnostics;
+# Getting a line of input from the keyboard.
+print "What is your name?  ";
+$name = <STDIN>;
+print "What is your father's name? ";
+$paname=<>;
+print "Hello respected one, $paname";
