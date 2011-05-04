1673239a14b0f06b6d1c4115f233d10e1a24f8ef
paul <daihuxyz@yahoo.com.cn>
2011-05-04 15:10:18 +0800

    bash

---
+++ chapter 5/0501.pl
@@ -0,0 +1,15 @@
+# Scalar, array, and hash assignment
+$salary=50000;                   # Scalar assignment
+@months=('Mar', 'Apr', 'May');   # Array assignment
+%states= (                       # Hash assignment
+    'CA' => 'California',
+	'ME' => 'Maine',
+	'MT' => 'Montana',
+	'NM' => 'New Mexico',
+	     );
+print "$salary\n";
+print "@months\n";
+print "$months[0], $months[1], $months[2]\n";
+print "$states{'CA'}, $states{'NM'}\n";
+print $x + 3, "\n" ;             # $x just came to life!
+print "***$name***\n" ;          # $name is born!
