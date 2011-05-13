93df78af737ed482178f7bff0249796a0c1d536d
paul <daihuxyz@yahoo.com.cn>
2011-05-13 16:04:04 +0800

    add autodevelopment

---
+++ autodevelopment.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use diagnostics;
+use Carp;
+my $stop= "/var/run/jsvc.pid";
+if( -s $stop){
+    print "Tomcat Service Staring Please Stoped Service \n";
+   }else{
+    print "Tomcat Serverce Stoped\n";
+}
+
+system("/etc/init.d/tomcat stop");
+system("unzip /usr/apache-tomcat-6.0.29/webapps/BizServ/WEB-INF/classes/com.zip -d /usr/apache-tomcat-6.0.29/webapps/BizServ/WEB-INF/classes/ ");
+system("/etc/init.d/tomcat start");
+
+
--- autoftpbackup.pl
+++ autoftpbackup.pl
@@ -4,7 +4,11 @@ $ftp = Net::FTP->new("174.121.21.12", Timeout => 30) or die "Could not connect.\
 $username = "daihu";
 $password = "3xzZCoidvs";
 $ftp->login($username, $password) or die "Could not log in.\n";
-$ftp->mkdir("²âÊÔ");
-$ftp->cwd("/²âÊÔ");
+$ftp->mkdir("developer");
+$ftp->cwd("/developer");
 $local="E:/com.zip";
-$ftp->put($local);
+$ftp->message();
+$ftp->
+$ftp->put($local)or die "Could not put remotefile:$local\n";
+$ftp->quit()
+
