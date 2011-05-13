76afb96091ee5138d541ee676a97473f31fd856d
paul <daihuxyz@yahoo.com.cn>
2011-05-13 17:13:44 +0800

    bashrc functional

--- Download.pl
+++ Download.pl
@@ -4,8 +4,9 @@ $ftp = Net::FTP->new("211.151.185.38", Timeout => 30) or die "Could not connect.
 $username = "eschange";
 $password = "eschange123";
 $ftp->login($username, $password) or die "Could not log in.\n";
+$ftp->binary();
 $remote= "com.zip";
-$local = "com.zip1";
+$local = "newcom.zip";
 $ftp->get($remote,$local)or die "Could not put remotefile:remote\n";
 $ftp->quit()
 
---
+++ bashrc
@@ -0,0 +1,16 @@
+# .bashrc
+
+# User specific aliases and functions
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+        . /etc/bashrc
+fi
+
+alias dr='cd /usr/apache-tomcat-6.0.29/webapps/ROOT'
+alias dl='cd /usr/apache-tomcat-6.0.29/logs'
+~                                                
