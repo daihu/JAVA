e2c4699dd6c6dfb6d990b10e4a96a4839b4f94f1
paul <daihuxyz@yahoo.com.cn>
2011-05-16 18:11:51 +0800

    perfect

--- autotomcat.pl
+++ autotomcat.pl
@@ -5,7 +5,7 @@ use diagnostics;
 use Carp;
 my $file= "/var/run/jsvc.pid";
 if( -s $file){
-    print "Tomcat Service started \n";
+    print "Tomcat Service Already  started \n";
    }else{
     print "Tomcat Starting \n";
     system("/etc/init.d/tomcat start")
