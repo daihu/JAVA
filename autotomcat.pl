c3c83235acde143ff2e923f5082c457b4e19d516
paul <daihuxyz@yahoo.com.cn>
2011-05-16 10:01:37 +0800

    自动控制Tomcat

---
+++ autotomcat.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use diagnostics;
+use Carp;
+my $file= "/var/run/jsvc.pid";
+if( -s $file){
+    print "Tomcat Service started \n";
+   }else{
+    print "Tomcat Starting \n";
+    system("/etc/init.d/tomcat start")
+}
