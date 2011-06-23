1ce564138da3c3e45e5b391c251847293c1fb23a
Administrator <Administrator@PAUL-PC>
2011-06-23 18:13:10 +0800

    openfetion

--- notice_fetion.pl
+++ notice_fetion.pl
@@ -12,3 +12,4 @@ my $Recipient="13911224264";
 my $content="北京市朝阳区东三环北路辛2号迪阳大厦1208室010-84536157转8009北京依思宸智信息技术有限公司";
 system("D:/bbs.it-adv.net/window/fetion.exe --mobile=$mobile --pwd=$password --to=$Recipient --msg-gb=$content ");
 
+system("/usr/local/Linux_X86/fetion --mobile=13911224264 --pwd=13810083864daihu --to=13911224264 --msg-utf8=ddd );
