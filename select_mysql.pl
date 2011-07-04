64b5c5cbe0ecc1f5981398964879d1aab42ab9a7
Administrator <Administrator@PAUL-PC>
2011-07-04 17:18:41 +0800

    修正无法连接数据库问题，下一步就是把IP地址改成域名的问题

--- select_mysql.pl
+++ select_mysql.pl
@@ -1,22 +1,30 @@
-#!/usr/bin/perl
-use warnings;
-use diagnostics;
-use Carp;
+#!/bin/perl
+
 use DBI;
+
+#use warnings;
+#use strict;
+#use utf8;
+#use bytes;
 
 # Connect to target DB
 my $dbh = DBI->connect( "DBI:mysql:database=zhazh;host=localhost",
-	"root", "111111", { 'RaiseError' => 1 } );
+	"root", "111111", { 'mysql_enable_utf8' => 1 } )
+  || die "Cannot connect to database:" . DBI->errstr;
+
 $dbh->do("set names gbk ");
 
 # query
-my $sqr = $dbh->prepare(
-	"select id ,resUrl,itemId,title from m_item_image
-);
+my $sqr = $dbh->prepare("select id ,itemId,resUrl,version from m_item_image");
 $sqr->execute();
 
- while ( my $ref = $sqr->fetchrow_hashref() ) {
- 	    print " $ref->n" ;
+while ( my $ref = $sqr->fetchrow_hashref() ) {
+	print "$ref->{'id'}\n";
+	print "$ref->{itemId}\n ";
+	print "$ref->{resUrl}\n ";
+	print "$ref->{version}\n ";
+	print "------------------------\n"
+
 }
 
 $dbh->disconnect();
