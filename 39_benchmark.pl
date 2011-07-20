9026b3ae2d0ed4c476111f87268bfcbf439a0f76
Administrator <Administrator@PAUL-PC>
2011-07-20 18:47:18 +0800

    commit file

--- 39_benchmark.pl
+++ 39_benchmark.pl
@@ -5,5 +5,7 @@ use Carp;
 
 #并发请求数据
 $concurrency_num=128;
+#迭代次数
+$iterations_num=3;
 
-/usr/local/mysql/bin/mysqlslap -a  --concurrency=128  --iterations=3  --number-of-queries=50000  -uroot  -peschange1208 -S /tmp/mysql.sock --debug-info
+#/usr/local/mysql/bin/mysqlslap -a  --concurrency=128  --iterations=3  --number-of-queries=50000  -uroot  -peschange1208 -S /tmp/mysql.sock --debug-info
