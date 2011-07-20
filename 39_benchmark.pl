8574a56b40791567f7889a558e0c5e09ea366ca3
Administrator <Administrator@PAUL-PC>
2011-07-20 18:44:14 +0800

    增加解释

--- 39_benchmark.pl
+++ 39_benchmark.pl
@@ -2,6 +2,8 @@
 use warnings;
 use diagnostics;
 use Carp;
-# use strict;
+
+#并发请求数据
+$concurrency_num=128;
 
 /usr/local/mysql/bin/mysqlslap -a  --concurrency=128  --iterations=3  --number-of-queries=50000  -uroot  -peschange1208 -S /tmp/mysql.sock --debug-info
