4398203500862774d47ff477d922360f955bec77
Administrator <Administrator@PAUL-PC>
2011-07-21 09:43:05 +0800

    修正无法查看部分

--- 39_benchmark.pl
+++ 39_benchmark.pl
@@ -7,5 +7,9 @@ use Carp;
 $concurrency_num=128;
 #µü´ú´ÎÊý
 $iterations_num=3;
-
-#/usr/local/mysql/bin/mysqlslap -a  --concurrency=128  --iterations=3  --number-of-queries=50000  -uroot  -peschange1208 -S /tmp/mysql.sock --debug-info
+system("/usr/local/mysql/bin/mysqlslap -a  
+--concurrency=128  
+--iterations=3  
+--number-of-queries=50000  
+-uroot  
+-peschange1208 -S /tmp/mysql.sock --debug-info")
