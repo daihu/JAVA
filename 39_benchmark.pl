caaa4379da18dfca9e0772e6bce378df5402183b
Administrator <Administrator@PAUL-PC>
2011-07-21 13:52:48 +0800

    写好了成功的脚本

--- 39_benchmark.pl
+++ 39_benchmark.pl
@@ -1,15 +1,19 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 use warnings;
 use diagnostics;
 use Carp;
 
-#并发请求数据
-$concurrency_num=128;
-#迭代次数
-$iterations_num=3;
-system("/usr/local/mysql/bin/mysqlslap -a  
---concurrency=128  
---iterations=3  
---number-of-queries=50000  
--uroot  
--peschange1208 -S /tmp/mysql.sock --debug-info")
+my $concurrency_num = "128";
+my $iterations_num = "3";
+my $queries_num="300000";
+my $username="root";
+my $password="eschange1208";
+my $enginetype="myisam";
+my $enginetype_i="innodb";
+my $database="zhazh";
+
+system  "/usr/local/mysql/bin/mysqlslap -a --concurrency=$concurrency_num  --iterations=$iterations_num --number-of-queries=$queries_num -u$username -p$password  --auto-generate-sql --auto-generate-sql-load-type=mixed --engine=$enginetype,$enginetype_i --auto-generate-sql-add-autoincrement --create-schema=$database --number-int-cols=4 --number-char-cols=35 -S /tmp/mysql.sock --debug-info ";
+
+
+
+ 
