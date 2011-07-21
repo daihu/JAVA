56a76a5d6cca7170665a55f34d713bb381e2ad45
Administrator <Administrator@PAUL-PC>
2011-07-21 15:44:29 +0800

    修正无法执行的错误，和相关参数调整

--- 39_benchmark.pl
+++ 39_benchmark.pl
@@ -12,7 +12,10 @@ my $enginetype="myisam";
 my $enginetype_i="innodb";
 my $database="zhazh";
 
-system  "/usr/local/mysql/bin/mysqlslap -a --concurrency=$concurrency_num  --iterations=$iterations_num --number-of-queries=$queries_num -u$username -p$password  --auto-generate-sql --auto-generate-sql-load-type=mixed --engine=$enginetype,$enginetype_i --auto-generate-sql-add-autoincrement --create-schema=$database --number-int-cols=4 --number-char-cols=35 -S /tmp/mysql.sock --debug-info ";
+system  "/usr/local/mysql/bin/mysqlslap --defaults-file=/etc/my.cnf --concurrency=$concurrency_num  --iterations=$iterations_num --number-of-queries=$queries_num -u$username -p$password  --auto-generate-sql --auto-generate-sql-load-type=mixed --engine=$enginetype,$enginetype_i --auto-generate-sql-add-autoincrement --create-schema=$database --number-int-cols=4 --number-char-cols=35 -S /tmp/mysql.sock --debug-info ";
+
+
+
 
 
 
