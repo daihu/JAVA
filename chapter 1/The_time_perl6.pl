ac78bda998a7fdc41c11d9c54406ffbd19760bc5
paul <daihuxyz@yahoo.com.cn>
2011-07-24 00:27:43 +0800

    中文输出

---
+++ chapter 1/The_time_perl6.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+use warnings;
+use diagnostics;
+use Carp;
+
+print "httt://dev.perl.org/perl6/ \n";
+print "Perl 5 was my rewrite of Perl.  I want Perl 6 to be the community's rewrite of Perl and of the community";
---
+++ chapter 1/what_is_perl.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/perl
+use warnings;
+print "Practical Extraction and Report Language\n";
+print "Pathologically Eclectic Rubbish Lister\n";
