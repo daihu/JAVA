c03ca02a541880e19f773d8870482359cb7d0d67
paul <daihuxyz@yahoo.com.cn>
2011-12-08 16:15:10 +0800

    cancel

--- ARGV/hour.pl
+++ ARGV/hour.pl
@@ -1,13 +1,13 @@
-* */1 * * *  perl /usr/local/src/forum.pl 44
+#* */1 * * *  perl /usr/local/src/forum.pl 44
 # minutes
-*/2 * * * *  perl /usr/local/src/forum_44.pl
+#*/2 * * * *  perl /usr/local/src/forum_44.pl
 
 
 #211.151.185.39
-*       6       *       *       *       /opt/backup/backup.pl
-*/5     *        *      *        *       php /usr/www/cacti.zhazh.com/poller.php > /dev/null 2>&1
-*/1     *        *       *       *        perl /usr/local/src/forum.pl 44
+#*       6       *       *       *       /opt/backup/backup.pl
+#*/5     *        *      *        *       php /usr/www/cacti.zhazh.com/poller.php > /dev/null 2>&1
+#*/1     *        *       *       *        perl /usr/local/src/forum.pl 44
 
 
 
-forum.pl 44
+#forum.pl 44
