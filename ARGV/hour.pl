29c62c8654d7bb4d0d9ea6a7dea18c02d04833be
paul <daihuxyz@yahoo.com.cn>
2011-12-09 14:33:32 +0800

    更新每天执行的

--- ARGV/hour.pl
+++ ARGV/hour.pl
@@ -1,7 +1,7 @@
 #* */1 * * *  perl /usr/local/src/forum.pl 44
 # minutes
 #*/2 * * * *  perl /usr/local/src/forum_44.pl
-
+#01 3 * * *	root /usr/sbin/ntpdate ntp.ubuntu.com
 
 #211.151.185.39
 #*       6       *       *       *       /opt/backup/backup.pl
