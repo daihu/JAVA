c04c8b840c74e265fb8cafa1ab021bb591eaf30e
paul <daihuxyz@yahoo.com.cn>
2012-02-08 16:55:13 +0800

    日志分析程序

---
+++ CGI/analytics_zhazh.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+#author:paul;
+use warnings;
+use diagnostics;
+use Carp;
+open (FH,"<localhost_access_log.2012-02-08.txt") or die "Couldn't open  for reading:$!";
+while ($line=<FH>){
+	 
+	}
+#close($line);
