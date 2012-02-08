5a487ca362d244eed759d85dd482e743f8184f80
paul <daihuxyz@yahoo.com.cn>
2012-02-08 17:21:20 +0800

    Tomcat 日志分析

--- CGI/analytics_zhazh.pl
+++ CGI/analytics_zhazh.pl
@@ -1,10 +1,13 @@
 #!/usr/bin/perl
-#author:paul;
 use warnings;
 use diagnostics;
 use Carp;
-open (FH,"<localhost_access_log.2012-02-08.txt") or die "Couldn't open  for reading:$!";
-while ($line=<FH>){
-	 
-	}
-#close($line);
+
+$data_file="localhost_access_log.2012-02-08.txt";
+open(FH,$data_file) || die("Could not open file!");
+ while(<FH>){
+ 	 print if m/200/ ;
+ 	 my $count++;
+ 	 print $count
+}
+     
